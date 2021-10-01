2093a82b60325e9bd772a622f3584d4873f8966a
Robin Khiv <robinkhiv@gmail.com>
2021-10-01 00:44:04 -0700

    updated with payment modal

--- scripts/formhandler.js
+++ scripts/formhandler.js
@@ -19,14 +19,29 @@
             // TODO: 
             this.$formElement.on('submit', function (event) {
                 event.preventDefault();
+        
+                $('#payment-modal').modal({
+                    escapeClose: false,
+                    clickClose: false,
+                    showClose: false
+                });
 
+                $('#payment-modal').on('submit', function(event){
+                    event.preventDefault();
+                    var data = {};
+                    $(this).serializeArray().forEach(function (item) { 
+                        data[item.name] = item.value;
+                    });
+                    const text = `<h2>Thank you for your payment, ${data.title} ${data.name}</h2><a href="#" rel="modal:close">Close</a>`;
+                    $('#payment-complete').html(text).modal();   
+                })
                 // var data = $(this).serializeArray();
                 var data = {};
                 $(this).serializeArray().forEach(function (item) { 
                     data[item.name] = item.value;
                     console.log(item.name + ' is ' + item.value);
                 });
-                console.log(data);
+                //console.log(data, 'data');
                 fn(data);         // truck createOrder is currently fn
                 this.reset();
                 this.elements[0].focus();
